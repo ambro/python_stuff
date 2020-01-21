d451490b06fd65fa5234a2226b515c55ca56b124
ambro <639921+ambro@users.noreply.github.com>
2020-01-21 10:36:02 +0100

    encapsulation, error handling and selenium wait

--- scraping-quotes/app.py
+++ scraping-quotes/app.py
@@ -1,15 +1,18 @@
 from selenium import webdriver
 from pages.quotes_page import QuotesPage
+from pages.quotes_page import InvalidTagForAuthorError
 
-chrome = webdriver.Chrome(executable_path='C:\workdir\python\chromedriver.exe')
-chrome.get('http://quotes.toscrape.com/search.aspx')
-page = QuotesPage(chrome)
+try:
+    chrome = webdriver.Chrome(executable_path='C:\workdir\python\chromedriver.exe')
+    chrome.get('http://quotes.toscrape.com/search.aspx')
+    page = QuotesPage(chrome)
 
+    author = input("Enter author:")
+    tag = input("Enter selected tag: ")
 
-author = input("Enter author:")
-page.select_author(author)
-tags = page.get_available_tags()
-print("Select one of tags[{}]".format(" | ".join(tags)))
-selected_tag = input("Enter selected tag: ")
-page.select_tag(selected_tag)
-page.search_button.click()
+    print(page.search_for_quotes(author, tag))
+except InvalidTagForAuthorError as e:
+    print(e)
+except Exception as e:
+    print(e)
+    print("Unknown error. Try again.")
--- scraping-quotes/pages/quotes_page.py
+++ scraping-quotes/pages/quotes_page.py
@@ -1,5 +1,9 @@
 from typing import List
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
 
 from locators.quotes_page_locators import QuotesPageLocators
 from parsers.quote import QuoteParser
@@ -37,3 +41,26 @@ class QuotesPage:
     @property
     def search_button(self):
         return self.browser.find_element_by_css_selector(QuotesPageLocators.SEARCH_BUTTON)
+
+    def search_for_quotes(self, author: str, tag: str) -> List[QuoteParser]:
+        self.select_author(author)
+
+        WebDriverWait(self.browser, 10).until(
+            expected_conditions.presence_of_all_elements_located(
+                (By.CSS_SELECTOR, QuotesPageLocators.TAG_DROPDOWN_VALUE_OPTION)
+            )
+        )
+
+        try:
+            self.select_tag(tag)
+        except NoSuchElementException:
+            raise InvalidTagForAuthorError(
+                f'Author "{author}" does not have any quotes tagged with "{tag}".'
+            )
+
+        self.search_button.click()
+        return self.quotes
+
+
+class InvalidTagForAuthorError(ValueError):
+    pass
